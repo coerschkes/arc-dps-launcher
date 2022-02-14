547d4d9f89f33ba5654a9c66a99915f7a5b74c09
Christian Oerschkes <christian.oerschkes@hotmail.de>
2022-02-14 15:22:23 +0100

    Refactoring of Dx9 implementation in preparation for Dx11 implementation

--- src/updater/ArcUpdaterDx9.go
+++ src/updater/ArcUpdaterDx9.go
@@ -14,25 +14,16 @@ import (
 */
 
 /*
-	The implementation of the ArcUpdater interface.
-
-	Needs a storage location for downloading the checksum file. In best case uses a temporary directory
-	that can be cleaned afterwards.
-
+	The implementation for the Dx9 installation of the ArcUpdater interface.
 */
 
-const binFolderPath = "bin64"
-
-type ArcUpdaterDx9 struct{}
-
-var logger logging.Logger
-
-func init() {
-	logger = logging.GetLogger("arcUpdaterImpl.go")
+type ArcUpdaterDx9 struct {
+	binFolderPath string
+	logger        logging.Logger
 }
 
 func NewArcUpdaterDx9() ArcUpdater {
-	return &ArcUpdaterDx9{}
+	return &ArcUpdaterDx9{"bin64", logging.GetLogger("arcUpdaterDx9.go")}
 }
 
 func (au ArcUpdaterDx9) IsInstalled() bool {
@@ -45,17 +36,17 @@ func (au ArcUpdaterDx9) IsUpToDate() bool {
 }
 
 func (au ArcUpdaterDx9) DownloadLatestVersion() {
-	logger.Log("Downloading latest arc-dps version")
+	au.logger.Log("Downloading latest arc-dps version")
 	utils.DownloadFile(d3d9Url, au.InstallationPath())
 }
 
 func (au ArcUpdaterDx9) DownloadChecksumFile() {
-	logger.Log("Downloading arcdps checksum file")
+	au.logger.Log("Downloading arcdps checksum file")
 	utils.DownloadFile(d3d9Md5Url, d3d9Md5Name)
 }
 
 func (au ArcUpdaterDx9) InstallationPath() string {
-	return binFolderPath + "\\" + d3d9Name
+	return au.binFolderPath + "\\" + d3d9Name
 }
 
 func (au ArcUpdaterDx9) RemoveChecksumFile() {
@@ -65,7 +56,7 @@ func (au ArcUpdaterDx9) RemoveChecksumFile() {
 func (au ArcUpdaterDx9) parseChecksum() string {
 	content, err := ioutil.ReadFile(d3d9Md5Name)
 	if err != nil {
-		logger.LogError(err)
+		au.logger.LogError(err)
 		panic(err)
 	}
 	return strings.Split(string(content), " ")[0]
